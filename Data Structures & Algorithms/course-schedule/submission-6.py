e35c81dc55d1c6df1aa37476bf3f9fff1c89886d
Paul Schneider <94721161+luap01@users.noreply.github.com>
2026-04-18 14:08:08 -0400

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = { c: [] for c in range(numCourses) } 
+
+        for c, pre in prerequisites:
+            graph[pre].append(c)
+        
+        
+        def dfs(crs):
+            if crs in visited:
+                return False
+    
+            visited.add(crs)
+            
+            for nxt in graph[crs]:
+                if not dfs(nxt):
+                    return False
+            
+            visited.remove(crs)
+            return True
+
+        for c in range(numCourses):
+            visited = set()
+            if not dfs(c):
+                return False
+
+        return True

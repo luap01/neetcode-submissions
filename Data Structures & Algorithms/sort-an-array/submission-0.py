ce6c61949bf8e4e95fb3832b3cd8133b814df976
Paul Schneider <94721161+luap01@users.noreply.github.com>
2026-04-19 15:12:45 -0400

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,53 @@
+class Solution:
+
+    # 10 9 1 1
+    # l=0, r=1, m=0 (10, 9)
+    # 
+    # 10 9
+    #
+    def merge(self, arr, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+
+        l1 = [0] * n1
+        l2 = [0] * n2
+
+        for i in range(n1):
+            l1[i] = arr[l + i]
+        for j in range(n2):
+            l2[j] = arr[m + 1 + j]
+        
+        i = 0  
+        j = 0  
+        k = l
+        while i < n1 and j < n2:
+            if l1[i] <= l2[j]:
+                arr[k] = l1[i]
+                i += 1
+            else:
+                arr[k] = l2[j]
+                j += 1 
+            k += 1
+        
+        while i < n1:
+            arr[k] = l1[i]
+            k += 1 
+            i += 1
+
+        while j < n2:
+            arr[k] = l2[j]
+            k += 1
+            j += 1
+    
+    def merge_sort(self, arr, l, r):
+        if l < r:
+            m = (l + r) // 2
+
+            self.merge_sort(arr, l, m)
+            self.merge_sort(arr, m + 1, r)
+            self.merge(arr, l, m, r)
+        
+    
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.merge_sort(nums, 0, len(nums) - 1)
+        return nums

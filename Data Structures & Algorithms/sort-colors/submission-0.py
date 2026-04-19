14d93adaa640110dff575da189634c1e4b36cb5b
Paul Schneider <94721161+luap01@users.noreply.github.com>
2026-04-19 15:21:38 -0400

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,47 @@
+class Solution:
+    def merge(self, arr, l, m, r):
+        n1 = m + 1 - l
+        n2 = r - m
+
+        lh = [0] * n1
+        rh = [0] * n2
+
+        for i in range(n1):
+            lh[i] = arr[l + i]
+        for j in range(n2):
+            rh[j] = arr[m + 1 + j]
+
+        i, j, k = 0, 0, l
+        while i < n1 and j < n2:
+            if lh[i] <= rh[j]:
+                arr[k] = lh[i]
+                i += 1
+            else:
+                arr[k] = rh[j]
+                j += 1
+            k += 1
+        
+        while i < n1:
+            arr[k] = lh[i]
+            i += 1
+            k += 1
+        
+        while j < n2:
+            arr[k] = rh[j]
+            j += 1
+            k += 1
+    
+    def merge_sort(self, arr, l, r):
+        if l < r:
+            m = (l + r) // 2
+
+            self.merge_sort(arr, l, m)
+            self.merge_sort(arr, m + 1, r)
+            self.merge(arr, l, m, r)
+
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        self.merge_sort(nums, 0, len(nums) - 1)
+        
